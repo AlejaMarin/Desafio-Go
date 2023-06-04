4d3ff060e5d26d277afc19b2146561d8d20ada21
vanemvilte90 <vanemvilte90@gmail.com>
2023-06-04 00:24:17 -0300

    Inicio Requerimiento 1 del Enunciado

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,52 @@
 package tickets
 
-type Ticket struct {
-}
+import (
+	
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int, error) {}
+
+func GetTotalTickets(destination string) (int, error) {
+	//Open CSV file
+	file, err := os.Open("tickets.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	//Read CSV file
+	rowData, err := os.ReadFile("./tickets.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	data := strings.Split(string(rowData), "\n")
+
+	total := 0
+	
+	for _, value := range data {
+		line := strings.Split(value, ",")
+		pais := line[3]
+		
+		if strings.EqualFold(pais, destination) {
+			total++
+		}
+	}
+
+	if total == 0 {
+		return 0, fmt.Errorf("pais no encontrado: %s", destination)
+	}
+
+	return total, nil
+	
+}
 
 // ejemplo 2
-func GetMornings(time string) (int, error) {}
+//func GetMornings(time string) (int, error) {}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int, error) {}
+//func AverageDestination(destination string, total int) (int, error) {}
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/AlejaMarin/Desafio-Go/internal/tickets"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	// Requerimiento 1
+	total, err := tickets.GetTotalTickets("china")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(total)
 }
